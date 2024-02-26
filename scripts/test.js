55a6654d0ad884d4204baad3cc7edc97c3258a96
LawrenceN3lson <hsc1325@outlook.com>
2024-02-26 18:00:37 +0800

    Add mathjs library and test script

---
+++ scripts/test.js
@@ -0,0 +1,11 @@
+const math = require("mathjs");
+
+// 定义变量和等式
+const eq1 = math.parse("x * y = 100");
+const eq2 = math.parse("x / y = 1 / 1.5");
+
+// 解等式
+const solution = math.solve([eq1, eq2], ["x", "y"]);
+
+console.log(`池子中 ETH 的数量：${solution.x}`);
+console.log(`池子中 DAI 的数量：${solution.y}`);
